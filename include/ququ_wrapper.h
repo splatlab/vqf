c61fecce79044fd395d3e6741d32d4f53dce60e2
Prashant Pandey <prashant.prashn@gmail.com>
2019-08-08 18:07:16 -0400

    fixing ququ wrapper.

--- include/ququ_wrapper.h
+++ include/ququ_wrapper.h
@@ -14,33 +14,33 @@
 
 #include "ququ_filter.h"
 
-ququ_filter q_filter;
+ququ_filter *q_filter;
 
 
 inline int q_init(uint64_t nbits)
 {
 	uint64_t nslots = (1ULL << nbits);
-	ququ_init(&q_filter, nslots);
+	q_filter = ququ_init(nslots);
 	return 0;
 }
 
 inline int q_insert(__uint128_t val)
 {
-	if (ququ_insert(&q_filter, val) < 0)
+	if (ququ_insert(q_filter, val) < 0)
 		return 0;
 	return 1;
 }
 
 inline int q_lookup(__uint128_t val)
 {
-	if (!ququ_is_present(&q_filter, val))
+	if (!ququ_is_present(q_filter, val))
 		return 0;
 	return 1;
 }
 
 inline __uint128_t q_range()
 {
-	return q_filter.metadata->range;
+	return q_filter->metadata.range;
 }
 
 inline int q_destroy()
